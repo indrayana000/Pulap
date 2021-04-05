2a16cd3681d8be33f398fe84ef13fc473994f4a7
indrayana000 <71499555+indrayana000@users.noreply.github.com>
2021-04-06 06:32:28 +0700

    Add files via upload

---
+++ Script/ConnectToServer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.SceneManagement;
+
+public class ConnectToServer : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        //starting the process of connecting to the server
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    //Success connected and load up Main Menu
+    public override void OnConnectedToMaster() {
+        SceneManager.LoadScene("Main Menu");
+    }
+
+}
